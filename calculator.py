ee1c9c734a26997fce528ce04a88fc108afccf11
khudeeva <kskhudeeva@gmail.com>
2025-03-07 14:16:28 +0700

    Добавлены тесты для работы со строками, списками, файлами, JSON

--- calculator.py
+++ calculator.py
@@ -15,4 +15,32 @@ def divide(a, b):
         return "Ошибка: деление на ноль"
     return a / b
 
+def capitalize_word(word):
+    if not isinstance(word, str):
+     return "Ошибка: не строка"
+    return word.capitalize()
+
+def remove_duplicates(lst):
+    if not isinstance(lst, list):
+     return "Ошибка: не список"
+    return list(dict.fromkeys(lst))
+
+import csv
+
+def save_list_to_csv(filename, data):
+    if not isinstance(data, list):
+     return "Ошибка: не список"
+    with open(filename, "w", newline="") as file:
+      writer = csv.writer(file)
+      writer.writerow(data)
+
+import json
+
+def save_json(filename, data):
+    with open(filename, "w") as file:
+        json.dump(data, file)
+
+def load_json(filename):
+    with open(filename, "r") as file:
+        return json.load(file)
 
--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,7 @@
 import unittest
-from calculator import add, subtract, multiply, divide, power
+import os
+import json
+from calculator import add, subtract, multiply, divide, power, capitalize_word, remove_duplicates, save_list_to_csv, save_json, load_json
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
@@ -32,6 +34,44 @@ class TestCalculator(unittest.TestCase):
     def test_power(self):
         self.assertEqual(power(2, 3), 8)  
         self.assertEqual(power(5, 0), 1)
+
+    def test_capitalize_word(self):
+        self.assertEqual(capitalize_word("hello"), "Hello")
+        self.assertEqual(capitalize_word("WORLD"), "World")
+        self.assertEqual(capitalize_word("123"), "123")
+        self.assertEqual(capitalize_word(100), "Ошибка: не строка")
+   
+    def test_remove_duplicates(self):
+        self.assertEqual(remove_duplicates([1, 2, 2, 3, 4, 4, 5]), [1, 2, 3, 4, 5])
+        self.assertEqual(remove_duplicates(["apple", "banana", "apple"]), ["apple", "banana"])
+        self.assertEqual(remove_duplicates([]), [])
+        self.assertEqual(remove_duplicates(123), "Ошибка: не список")
+ 
+    import os
+
+    def test_save_list_to_csv(self):
+        filename = "test_output.csv"
+        test_data = ["apple", "banana", "cherry"]
+
+        save_list_to_csv(filename, test_data)
+
+        with open(filename, "r") as file:
+            content = file.read().strip()
+
+        self.assertEqual(content, "apple,banana,cherry")
+
+        os.remove(filename)
+    
+    def test_json_operations(self):
+        filename = "test.json"
+        test_data = {"name": "Alice", "age": 25}
+
+        save_json(filename, test_data)
+        result = load_json(filename)
+
+        self.assertEqual(result, test_data)
+
+        os.remove(filename)
 
 if __name__ == '__main__':
     unittest.main()
